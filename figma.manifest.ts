85fd1ad9530bfeb0d4e274e6c926411616478e2f
Juniper1106 <zhuoyicheng1106@gmail.com>
2024-09-22 22:41:24 +0800

    modify id & update config

--- figma.manifest.ts
+++ figma.manifest.ts
@@ -1,10 +1,17 @@
 // https://www.figma.com/plugin-docs/manifest/
 export default {
   name: "Figma-Plugin-React-Vite",
-  id: "1222852692367737510",
+  id: "1419651496336343854",
   api: "1.0.0",
   main: "plugin.js",
   ui: "index.html",
+  "documentAccess": "dynamic-page",
+  "networkAccess": {
+    "allowedDomains": [
+      "*"
+    ],
+    "reasoning": "dev"
+  },
   capabilities: [],
   enableProposedApi: false,
   editorType: ["figma", "figjam"],
